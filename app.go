fefe3675ceca3234487f4c7faf49426a7271b8c4
6LYTH3 <ST4R13UCK@gmail.com>
2016-12-11 16:34:59 +0700

    Move to route

--- app.go
+++ app.go
@@ -1,43 +1,14 @@
 package main
 
 import (
-	controller "./controller"
-	user "./model/user"
-	"encoding/json"
-	"fmt"
-	"github.com/julienschmidt/httprouter"
+	"./route"
 	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
-
-func UserHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	u := user.User{
-		Name:   "Bob Smith",
-		Gender: "male",
-		Age:    25,
-		Id:     p.ByName("id"),
-	}
-
-	uj, _ := json.Marshal(u)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	fmt.Fprintf(w, "%s", uj)
-}
 
 func main() {
 	log.Printf("Come on baby, I'm listening you on port 8080!")
 
-	uc := controller.NewUserController()
-
-	r := httprouter.New()
-	// A handler
-	r.GET("/", handler)
-	r.GET("/user/:id", UserHandler)
-	r.GET("/xxx/:id", uc.GetUser)
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":8080", route.Load())
 }
---
+++ route/route.go
@@ -0,0 +1,42 @@
+package route
+
+import (
+  controller "../controller"
+  user "../model/user"
+  "encoding/json"
+  "fmt"
+  "github.com/julienschmidt/httprouter"
+  "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func UserHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	u := user.User{
+		Name:   "Bob Smith",
+		Gender: "male",
+		Age:    25,
+		Id:     p.ByName("id"),
+	}
+
+	uj, _ := json.Marshal(u)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "%s", uj)
+}
+
+
+func Load() *httprouter.Router  {
+  uc := controller.NewUserController()
+
+  r := httprouter.New()
+  // A handler
+  r.GET("/", handler)
+  r.GET("/user/:id", UserHandler)
+  r.GET("/xxx/:id", uc.GetUser)
+
+  return r
+}
